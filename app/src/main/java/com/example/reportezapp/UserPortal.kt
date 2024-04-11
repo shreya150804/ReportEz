308ccd1a76919a80267a3f875d568d286c8d1826
shreya150804 <132338964+shreya150804@users.noreply.github.com>
2024-04-11 22:46:20 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/reportezapp/UserPortal.kt
@@ -0,0 +1,10 @@
+package com.example.reportezapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserPortal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
