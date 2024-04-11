0d976cb8ac0f55edd4580a5faa16d44631937357
Shreya <shreyawani2018@gmail.com>
2024-04-11 10:49:31 +0530

    new version

---
+++ app/src/main/java/com/example/reportezapp/CreateAcc.kt
@@ -0,0 +1,10 @@
+package com.example.reportezapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateAcc : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/reportezapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.reportezapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val signin = findViewById<Button>(R.id.signin)
+        signin.setOnClickListener( View.OnClickListener() {
+            fun onClick(view: View?) {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        val createacc = findViewById<Button>(R.id.createacc)
+        signin.setOnClickListener( View.OnClickListener() {
+                fun onClick(view: View?) {
+                    val intent = Intent(this, CreateAcc::class.java)
+                    startActivity(intent)
+                }
+    })
+})}}
