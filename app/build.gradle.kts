1d7fd1f18c70c175533fd17b3854ead74b2054c7
Shreya <shreyawani2018@gmail.com>
2025-11-22 17:32:15 +0530

    Clean repo and secure API keys

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        manifestPlaceholders["GOOGLE_API_KEY"] =
+            project.properties["GOOGLE_API_KEY"]?.toString() ?: ""
+
+
     }
 
     buildTypes {
